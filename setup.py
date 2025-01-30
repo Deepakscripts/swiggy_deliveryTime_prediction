ed496f6350d35882aacf46489bf846546028f3d9
Deepak agrawal <aadeepak05@gmail.com>
2025-01-30 13:10:31 +0530

    our first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Your project analyzes Swiggy delivery times to understand patterns, optimize efficiency, and improve predictions. It involves collecting real-time or historical delivery data, identifying key factors affecting delivery speed (e.g., distance, traffic, restaurant prep time), and using data analytics or machine learning to enhance accuracy. The goal is to provide insights for better customer experience and operational efficiency.',
+    author='Deepak Agrawal',
+    license='',
+)
